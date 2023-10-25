d43408d0cd1cc3723f0c003e465d13df86c9bce3
jmato <jmatosfernandes@live.com.pt>
2023-10-25 15:19:00 +0100

    hello

--- app.py
+++ app.py
@@ -1,39 +1,81 @@
-import webbrowser
+import json
+import time
 from xml.dom import minidom
 from xml.etree.ElementTree import Element, SubElement, tostring
 import requests
 import shopify
 from bs4 import BeautifulSoup
-from flask import Flask, request
 
-app = Flask(__name__)
+api_request_delay = 2
+
+ACCESS_TOKEN = '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'
+API_VERSION = '2023-10'
+API_KEY = '2c39bf093b7fd195f0484847a65a2648'
+PASSWORD = '537bbbeae234e1a815a43d617e1aa8da'
 
 
-@app.route('/index', methods=["GET", "POST"])
-def generate_feed():
-    # call the feed_generator function to generate the XML file
-    tag = request.args.get("tag")
-    main()
-    # render the template and pass the generated xml file
-    xml_feed = main()
-    return xml_feed, 200, {'Content-Type': 'text/xml'}
+def fetch_and_save_products_by_tag(shopify_store_url, tag, output_file, request_timeout=10):
+    global response
+    page = 1
+    product_data = []
 
+    while True:
+        shopify.ShopifyResource.clear_session()
+        shopify.ShopifyResource.site = shopify_store_url
+        shopify.ShopifyResource.headers = {}
+
+        # Construct the API endpoint with pagination and specify the API version
+        api_endpoint = f"/admin/api/products.json?tags={tag}&limit=250&page={page}"
+
+        # Make a GET request to the Shopify API with a timeout, using API key and password
+        full_url = f"{shopify_store_url}{api_endpoint}"
+        try:
+            print(full_url)
+            response = requests.get(full_url, auth=(API_KEY, PASSWORD))
+        except requests.exceptions.Timeout:
+            print("Request timed out. Retrying in a moment...")
+            time.sleep(5)  # Sleep for 5 seconds and then retry
+            continue
+
+        # Check if the request was successful
+        if response.status_code == 200:
+            page_data = response.json().get('products')
+            if not page_data:
+                break  # No more pages to fetch
+            product_data.extend(page_data)
+            page += 1
+        elif response.status_code == 401:
+            print("Authentication failed. Please check your API key and password.")
+            break
+        else:
+            print(f"Failed to retrieve product data. Status code: {response.status_code}")
+            break
+
+        # Introduce a delay to stay within rate limits
+        time.sleep(api_request_delay)
+
+    # Save all product data to the specified output file
+    with open(output_file, 'w') as file:
+        json.dump(product_data, file, indent=4)
+
+    if response.status_code == 200:
+        print(f"All products with the tag '{tag}' have been copied to '{output_file}'.")
 
 def main():
-    # Shopify API credentials
     tag = "KuantoKusta"
-    # shop_url = "https://abc-escolar.myshopify.com"
     api_key = "532e586c6a52981c06caa7eeec38ee8c"
     access_token = "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"
     password = "79686ebe8e57a554f1c0d89ddf8503a5"
+    output_file = 'products.json'
+    shopify_store_url = 'https://abc-escolar.myshopify.com'  # Remove "https://"
     headers = {
         "Content-Type": "application/json",
         "X-Shopify-Access-Token": access_token
     }
-    shopify.ShopifyResource.set_site("https://abc-escolar.myshopify.com")
+    shopify.ShopifyResource.set_site("https://abcescolar.pt")
     products_url = f"https://abc-escolar.myshopify.com/admin/products.json"
     response = requests.get(products_url, auth=(api_key, password), headers=headers)
-    # data = response.json()
+    fetch_and_save_products_by_tag(shopify_store_url, tag, output_file)
     page = 1
     per_page = 250
 
@@ -46,33 +88,39 @@ def main():
 
         for product in products:
             if tag in product.tags:
-                # create a new element for the product
-                product_element = SubElement(root, "product")
-
-                # add the title, sku, and vendor of the product as subelements
-                SubElement(product_element, "upc_ean").text = str(product.variants[0].sku)
-                SubElement(product_element, "reference").text = str(product.variants[0].sku)
-                SubElement(product_element, "brand").text = str(product.vendor)
-                SubElement(product_element, "category").text = str(product.product_type)
-                SubElement(product_element, "designation").text = str(product.title)
-                SubElement(product_element, "regular_price").text = product.variants[0].price + "€"
-                SubElement(product_element, "current_price").text = ""
-                SubElement(product_element, "availability").text = "in stock"
-                SubElement(product_element, "stock").text = "5"
-                SubElement(product_element, "norma_shipping_cost").text = "5.00€"
-                SubElement(product_element, "min_delivery_time").text = "1 dia"
-                SubElement(product_element, "max_delivery_time").text = "5 dia"
-                SubElement(product_element, "product_url").text = "https://abcescolar.pt/products/" + product.handle
-                description = BeautifulSoup(product.body_html, "html.parser").get_text()
-                SubElement(product_element, "description").text = description
-
-                # ET.SubElement(product_element, "description").text = str(product.body_html)
-                SubElement(product_element, "image_url").text = product.images[0].src
-                SubElement(product_element, "size").text = "S"
-                SubElement(product_element, "weight").text = "1Kg"
-                SubElement(product_element, "quantity").text = "1"
-                SubElement(product_element, "color").text = "variadas"
-                SubElement(product_element, "aux").text = "variadas"
+                for variant in product.variants:
+                    product_element = SubElement(root, "product")
+                    if variant.title == "Default Title":
+                        SubElement(product_element, "designation").text = str(product.title)
+                    else:
+                        SubElement(product_element, "designation").text = str(product.title + " " + variant.title)
+                    SubElement(product_element, "upc_ean").text = str(variant.sku)
+                    SubElement(product_element, "reference").text = str(variant.sku)
+                    # SubElement(product_element, "brand").text = str(product.vendor)
+                    # SubElement(product_element, "category").text = str(product.product_type)
+                    # SubElement(product_element, "category2").text = str(product.tags)
+                    if product.variants[0].compare_at_price is None:
+                        SubElement(product_element, "regular_price").text = str(variant.price) + "€"
+                    else:
+                        SubElement(product_element, "regular_price").text = str(
+                            variant.compare_at_price) + "€"
+                    SubElement(product_element, "current_price").text = str(variant.price) + "€"
+                    # SubElement(product_element, "availability").text = "in stock"
+                    # SubElement(product_element, "stock").text = "5"
+                    # SubElement(product_element, "norma_shipping_cost").text = "5€"
+                    # SubElement(product_element, "min_delivery_time").text = "1 dia"
+                    # SubElement(product_element, "max_delivery_time").text = "5 dia"
+                    # SubElement(product_element,
+                    #          "product_url").text = "https://abcescolar.pt/products/" + product.handle
+                    description = BeautifulSoup(product.body_html, "html.parser").get_text()
+                    SubElement(product_element, "description").text = description
+                    # ET.SubElement(product_element, "description").text = str(product.body_html)
+                    # SubElement(product_element, "image_url").text = product.images[0].src
+                    # SubElement(product_element, "size").text = "S"
+                    # SubElement(product_element, "weight").text = str(product.variants[0].grams) + " g"
+                    # SubElement(product_element, "quantity").text = "1"
+                    # SubElement(product_element, "color").text = "variadas"
+                    # SubElement(product_element, "aux").text = "variadas"
 
         # check if there are more products to paginate through
         if len(products) < per_page:
@@ -94,8 +142,5 @@ def main():
     return xml_str
 
 
-main()
-
 if __name__ == '__main__':
-    app.run()
-webbrowser.open("http:127.0.0.1:5000/index")
+    main()
